608e012f53f708a00fb536ff7cb9b689fe8f695c
Caio-Caetano <113916728+Caio-Caetano@users.noreply.github.com>
2023-04-20 13:58:50 -0300

    Update main.py
    Add bill-info route : returns a Json with due_date, value, cod_bar and file_name
    Update regex_value to accepts higher numbers like 1000+

--- main.py
+++ main.py
@@ -18,6 +18,37 @@ def index():
     <p> Thanks! :D </p>
     """
 
+@app.route('/bill-info', methods=['POST'])
+def get_bill_info():
+    files = request.files.getlist('files')
+
+    regex_cod_bar = r'\d{5}\.\d{5}\s\d{5}\.\d{6}\s\d{5}\.\d{6}\s\d\s\d{14}'
+    regex_due_date = r'(\d{2}/\d{2}/\d{4})'
+    regex_value = r'(\d{1,3}(?:\.\d{3})*,\d{2})'
+
+    for oneFile in files:
+        pdf_bytes = oneFile.read()
+        pdf_stream = io.BytesIO(pdf_bytes)
+        pdf_reader = PdfReader(pdf_stream)
+        page_content = pdf_reader.pages[0].extract_text()
+
+        due_date_match = re.search(regex_due_date, page_content)
+        value_match = re.search(regex_value, page_content)
+        cod_bar_match = re.search(regex_cod_bar, page_content)
+
+        due_date = due_date_match.group(1) if due_date_match else None
+        value = value_match.group(1) if value_match else None
+        cod_bar = cod_bar_match.group().replace('\n', '') if cod_bar_match else None
+        
+        dic_return = {
+            'due_date': due_date,
+            'value': value,
+            'cod_bar': cod_bar,
+            'file_name':oneFile.filename
+        }
+
+    return dic_return
+
 @app.route('/bill-upload', methods=['POST'])
 def vincular_arquivos():
     residents_json = request.form.get('residents')
@@ -26,7 +57,7 @@ def vincular_arquivos():
 
     regex_cod_bar = r'\d{5}\.\d{5}\s\d{5}\.\d{6}\s\d{5}\.\d{6}\s\d\s\d{14}'
     regex_due_date = r'(\d{2}/\d{2}/\d{4})'
-    regex_value = r'(\d+,\d{2})'
+    regex_value = r'(\d{1,3}(?:\.\d{3})*,\d{2})'
 
     uploadSuccessfully = []
     uploadUnsuccessfuly = residents_list
@@ -51,7 +82,7 @@ def vincular_arquivos():
                 uploadSuccessfully.append({'user':user, 'cod_bar':cod_bar, 'value':value, 'due_date':due_date, 'file_name':oneFile.filename})
                 uploadUnsuccessfuly.remove(user)
     
-    return jsonify({'success':uploadSuccessfully, 'unsuccess':uploadUnsuccessfuly})
+    return {'success':uploadSuccessfully, 'unsuccess':uploadUnsuccessfuly}
 
 if __name__ == '__main__':
     app.run()
